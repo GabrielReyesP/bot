01175f6eb88552088ec9f7f1f03cd8dd7a4e699a
GabrielReyesP <91704267+GabrielReyesP@users.noreply.github.com>
2025-06-02 01:45:27 -0400

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,24 @@
+<?php
+$update = json_decode(file_get_contents("php://input"),true);
+$message = $update ['message'] ?? null;
+
+if($message){
+    $text = strtolower(trim($message['text'] ?? ''));
+    $chatId = $message['chat']['id'] ?? '';
+    $productos =[
+        'carne'&&'queso'&&'jamon' => 'Pasillo 1',
+        'leche'&&'Yogurth'&&'Cereal'=> 'pasillo 2',
+        'Bebidas'&&'Jugos' => 'pasillo 3',
+        'Pan'&&'Pasteles'&&'Tortas' => 'Pasillo 4',
+        'Detergente'&&'Lavaloza' =>'Pasillo 5'
+    ];
+    if($text === '/start'){
+        $response = "¡Hola! Bienvenido al asistente virtual del supermercado \n\npuedes";
+    } else {
+        $response = $productos[$text] ?? "Lo siento, no entiendo lo que quieres decir";
+    }
+    $botToken = "7553138734:AAEyLBFufqhstjus_kyeKMxv0zxXQ2-1r30";
+    $apiUrl = "https://web.telegram.org/a/#7553138734";
+    file_get_contents($apiUrl . "?chat_id={$chatId}&text=" . urldecode($response));
+
+}   
